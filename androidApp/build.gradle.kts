68b61f4bb8963cdb313bb06f036b5a0c2afe1406
Régis <regis-971@hotmail.com>
2025-09-16 14:54:48 +0200

    Update to 1.2 and fix crash deseiralization

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.idsolution.icondoapp.android"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.2"
         // Add NDK configuration
         ndk {
             abiFilters.addAll(listOf("arm64-v8a", "armeabi-v7a"))
