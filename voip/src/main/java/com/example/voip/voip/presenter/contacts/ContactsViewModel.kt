2d0c952605295de6e5e442ab20f32ff4e8da7682
Régis <regis-971@hotmail.com>
2025-10-13 14:45:35 +0200

    Updates SIP server address
    Updates the SIP server address to sip.linphone.org.
    This ensures calls are routed correctly.

--- voip/src/main/java/com/example/voip/voip/presenter/contacts/ContactsViewModel.kt
+++ voip/src/main/java/com/example/voip/voip/presenter/contacts/ContactsViewModel.kt
@@ -14,7 +14,7 @@ class ContactsViewModel(
 ) : ViewModel() {
 
     fun callNumber(number: String) {
-        voip.outgoingCall("sip:$number@31.97.155.55")
+        voip.outgoingCall("sip:$number@sip.linphone.org")
     }
 
     fun logout() {
