9062492442f2ee09f70ac591d1f4eed4a105df97
Régis <regis-971@hotmail.com>
2025-10-13 14:45:50 +0200

    Updates default VOIP credentials to test
    Updates the default VOIP username, password, and domain in the login state.
    This change uses test credentials for the VOIP service to enable testing.

--- shared/src/commonMain/kotlin/com/idsolution/icondoapp/feature/auth/presentation/login/LoginState.kt
+++ shared/src/commonMain/kotlin/com/idsolution/icondoapp/feature/auth/presentation/login/LoginState.kt
@@ -6,9 +6,9 @@ import com.idsolution.icondoapp.feature.ssh.data.models.sites.SiteDto
 data class LoginState(
     val email: TextFieldState = TextFieldState("demo@fitz.com"),
     val password: TextFieldState = TextFieldState("demoFitz1!"),
-    val voipUsername: TextFieldState = TextFieldState("1001"),
-    val voipPassword: TextFieldState = TextFieldState("idSolution123!!"),
-    val voipDomain: TextFieldState = TextFieldState("31.97.155.55"),
+    val voipUsername: TextFieldState = TextFieldState("regis_test"),
+    val voipPassword: TextFieldState = TextFieldState("e1d2o3U4"),
+    val voipDomain: TextFieldState = TextFieldState("sip.linphone.org"),
     val isPasswordVisible: Boolean = false,
     val isVoipPasswordVisible: Boolean = false,
     val canLogin: Boolean = true,
